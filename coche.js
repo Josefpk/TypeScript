51af19efe5391c49c15363f4ca2607d9a130854c
Josefpk <josef.ban.v@gmail.com>
2017-09-10 18:43:00 -0500

    POO, creando clases, atributos y metodos parte 2

--- coche.js
+++ coche.js
@@ -1,11 +1,21 @@
 var Coche = /** @class */ (function () {
     function Coche() {
+        this.veloocidad = 0;
     }
     Coche.prototype.getColor = function () {
         return this.color;
     };
     Coche.prototype.setColor = function (color) {
         this.color = color;
+    };
+    Coche.prototype.acelerar = function () {
+        this.veloocidad++;
+    };
+    Coche.prototype.frenar = function () {
+        this.veloocidad--;
+    };
+    Coche.prototype.getVelocidad = function () {
+        return this.veloocidad;
     };
     return Coche;
 }());
@@ -14,8 +24,14 @@ var coche = new Coche();
 var coche2 = new Coche();
 var coche3 = new Coche();
 coche.setColor("ROJO");
-coche2.setColor("AZUL");
-coche3.setColor("VERDE");
+/*coche2.setColor("AZUL");
+coche3.setColor("VERDE"); */
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
 console.log("El color del coche uno es: " + coche.getColor());
-console.log("El color del coche uno es: " + coche2.getColor());
-console.log("El color del coche uno es: " + coche3.getColor());
+console.log("La velocidad del coche uno es: " + coche.getVelocidad());
+coche.frenar();
+console.log("La velocidad del coche uno despues de frenar es: " + coche.getVelocidad());
+/*console.log("El color del coche uno es: "+ coche2.getColor());
+console.log("El color del coche uno es: "+ coche3.getColor());*/ 
--- coche.ts
+++ coche.ts
@@ -1,7 +1,7 @@
 class Coche{
 	public color: string;
 	public modelo: string;
-	public veloocidad: number;
+	public veloocidad: number = 0;
 
 	public getColor(){
 		return this.color;
@@ -11,6 +11,18 @@ class Coche{
 	public setColor(color: string){
 		this.color = color;
 	}
+
+	public acelerar(){
+		this.veloocidad++;
+	}
+
+	public frenar(){
+		this.veloocidad--;
+	}
+	public getVelocidad(){
+		return this.veloocidad;
+
+	}
 }
 
 //CREAR OBJETO
@@ -18,10 +30,22 @@ class Coche{
 var coche = new Coche();
 var coche2 = new Coche();
 var coche3 = new Coche();
+
 coche.setColor("ROJO");
-coche2.setColor("AZUL");
-coche3.setColor("VERDE");
+/*coche2.setColor("AZUL");
+coche3.setColor("VERDE"); */
+
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
 
 console.log("El color del coche uno es: "+ coche.getColor());
-console.log("El color del coche uno es: "+ coche2.getColor());
-console.log("El color del coche uno es: "+ coche3.getColor());
+console.log("La velocidad del coche uno es: "+ coche.getVelocidad());
+
+coche.frenar();
+
+console.log("La velocidad del coche uno despues de frenar es: "+ coche.getVelocidad());
+
+
+/*console.log("El color del coche uno es: "+ coche2.getColor());
+console.log("El color del coche uno es: "+ coche3.getColor());*/
